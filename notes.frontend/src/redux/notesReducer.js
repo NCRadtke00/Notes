08403ac154869527bb2c30ff6f277f93ccaafed9
Nicholas Radtke <Raddnc@gmail.com>
2021-05-31 13:12:17 -0500

    working on deleting notes

--- notes.frontend/src/redux/notesReducer.js
+++ notes.frontend/src/redux/notesReducer.js
@@ -3,6 +3,7 @@ const initialState = {
 };
 export const ActionTypes = {
   SET_NOTES: "SET_NOTES",
+  DELETE_NOTE: "DELETE_NOTE",
 };
 export const ActionCreators = {
   setNotes: (payload) => ({ type: ActionTypes.SET_NOTES, payload }),
@@ -11,6 +12,14 @@ export default function NotesReducer(state = initialState, action) {
   switch (action.type) {
     case ActionTypes.SET_NOTES:
       return { ...state, notes: [...action.payload] };
+    case ActionTypes.DELETE_NOTE:
+      for (let i = 0; i < state.notes.length; i++) {
+        if (state.notes[i].id === action.payload.id) {
+          state.notes.splice(i, 1);
+          break;
+        }
+      }
+      return { ...state, notes: [...state.notes] };
     default:
       return state;
   }
